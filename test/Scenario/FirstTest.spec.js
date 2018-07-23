1e67bc18405a71cb88c4971145e832bbf7052722
Lodo4ka <doomymax@gmail.com>
2018-07-23 23:16:21 +0300

    fix selenium and mocha conflict

--- test/Scenario/FirstTest.spec.js
+++ test/Scenario/FirstTest.spec.js
@@ -1,67 +1,67 @@
-const webdriver = require('selenium-webdriver');
-const Chrome = require('selenium-webdriver/chrome');
-const By = webdriver.By;
-const until = webdriver.until;
-const expect = require('chai').expect;
-const assert = require('chai').assert;
-const Key = webdriver.Key;
-const WebElement = webdriver.WebElement;
-const optionsChrome = new Chrome.Options();
-const chaiAsPromised = require('chai-as-promised');
-const chai = require('chai');
-const should = chai.should();
-const login = require('../../config/login').login;
-const password = require('../../config/login').password;
-const url = require('../../config/login').baseUrl;
-const nameinput = require('../../utils/selectors.js').nameinput;
-const passinput = require('../../utils/selectors.js').passinput;
-const submitBtn = require('../../utils/selectors.js').submitBtn;
-const chaiWebdriverExec = require('chai-webdriver-exec');
+// const webdriver = require('selenium-webdriver');
+// const Chrome = require('selenium-webdriver/chrome');
+// const By = webdriver.By;
+// const until = webdriver.until;
+// const expect = require('chai').expect;
+// const assert = require('chai').assert;
+// const Key = webdriver.Key;
+// const WebElement = webdriver.WebElement;
+// const optionsChrome = new Chrome.Options();
+// const chaiAsPromised = require('chai-as-promised');
+// const chai = require('chai');
+// const should = chai.should();
+// const login = require('../../config/login').login;
+// const password = require('../../config/login').password;
+// const url = require('../../config/login').baseUrl;
+// const nameinput = require('../../utils/selectors.js').nameinput;
+// const passinput = require('../../utils/selectors.js').passinput;
+// const submitBtn = require('../../utils/selectors.js').submitBtn;
+// const chaiWebdriverExec = require('chai-webdriver-exec');
 
 
-let driver;
+// let driver;
 
-chai.use(chaiWebdriverExec(driver));
+// chai.use(chaiWebdriverExec(driver));
 
-describe('test for crm2', function() {
-    before(function() {
-        driver = new webdriver.Builder()
-            .withCapabilities(webdriver.Capabilities.chrome())
-            .setChromeOptions(optionsChrome)
-            .build();
-        driver.get(url);
-        console.log('Selenium Webdriver Chrome Started');
-    });
+// describe('test for crm2', function() {
+//     before(function() {
+//         driver = new webdriver.Builder()
+//             .withCapabilities(webdriver.Capabilities.chrome())
+//             .setChromeOptions(optionsChrome)
+//             .build();
+//         driver.get(url);
+//         console.log('Selenium Webdriver Chrome Started');
+//     });
 
-    after(function() {
-        // driver.quit();
-    });
+//     after(function() {
+//         // driver.quit();
+//     });
 
-    describe('isLogin', function() {
-        it('write valid login', function() {
-         
-            // driver.findElement(By.name(passinput)).sendKeys(password).then((result) => {
-            //     console.log(result);
-            // }).catch((err) => {
-            //     console.log(err);
-            // });
-            driver.findElement(By.css('#Myid')).click()
-                .then((result) => {
-                    console.log(result);
-                }).catch((err) => {
-                    console.log(err);
-                });
-        });
-        
-    });
+//     describe('isLogin', function() {
+//         it('write valid login', function() {
 
-    // it('write valid login', function() {
-    //     driver.findElement(By.name(selectors.nameinput)).sendKeys(login);
-    //     driver.findElement(By.name(selectors.passinput)).sendKeys(password);
-    //     driver.findElement(By.css(selectors.submitBtn)).click();
-    // });
+//             // driver.findElement(By.name(passinput)).sendKeys(password).then((result) => {
+//             //     console.log(result);
+//             // }).catch((err) => {
+//             //     console.log(err);
+//             // });
+//             driver.findElement(By.css('#Myid')).click()
+//                 .then((result) => {
+//                     console.log(result);
+//                 }).catch((err) => {
+//                     console.log(err);
+//                 });
+//         });
 
-    // it('should work as expected', function() {
-    //     chai.expect('return window.scrollX').to.be.a(1);
-    // });
-});
+//     });
+
+//     // it('write valid login', function() {
+//     //     driver.findElement(By.name(selectors.nameinput)).sendKeys(login);
+//     //     driver.findElement(By.name(selectors.passinput)).sendKeys(password);
+//     //     driver.findElement(By.css(selectors.submitBtn)).click();
+//     // });
+
+//     // it('should work as expected', function() {
+//     //     chai.expect('return window.scrollX').to.be.a(1);
+//     // });
+// });
--- test/Scenario/Yandex.spec.js
+++ test/Scenario/Yandex.spec.js
@@ -1,25 +1,53 @@
 const webdriver = require('selenium-webdriver');
 const Chrome = require('selenium-webdriver/chrome');
+const { it, after, before, describe } = require('selenium-webdriver/testing');
+const util = webdriver.until;
 const By = webdriver.By;
 // const optionsChrome = new Chrome.Options();
 let driver;
+// describe.only('test for yandex', function() {
+//     before(function() {
+//         driver = new webdriver.Builder()
+//             .forBrowser('chrome')
+//             .build();
+//         driver.get('https://www.yandex.ru/');
+//         console.log('Selenium Webdriver Chrome Started');
+//     });
 
-describe.only('test for yandex', function() {
-    before(function () {
-        driver = new webdriver.Builder()
-            .forBrowser('chrome')
-            .build();
+//     after(function() {
+//         driver.quit();
+//     });
+
+//     it('search by yandex', function() {
+//         driver.findElement((By.css('#text'))).then((result) => {
+//             console.log('succes' + result);
+//         }).catch((err) => {
+//             console.log(err);
+//         });
+//     });
+// });
+
+describe('test for yandex', () => {
+    before(function() {
+        this.timeout(15000);
+        driver = new webdriver.Builder().forBrowser('chrome').build();
         driver.get('https://www.yandex.ru/');
-        console.log('Selenium Webdriver Chrome Started');
     });
 
-    after(function () {
+    after(function() {
         driver.quit();
     });
 
-    it('search by yandex', function() {
-        driver.findElement((By.css('.input__control'))).then((result) => {
-            console.log('succes' + result);
+    it('search for yandex', function(done) {
+        this.timeout(15000);
+        setTimeout(done, 15000);
+        driver.findElement(By.css('#text')).sendKeys('bla').then((result) => {
+            console.log(result);
+        }).catch((err) => {
+            console.log(err);
+        });
+        driver.findElement(By.css('button[type="submit"]')).click().then((result) => {
+            console.log(result);
         }).catch((err) => {
             console.log(err);
         });
